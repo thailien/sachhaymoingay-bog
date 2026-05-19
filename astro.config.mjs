e92eda17bf33df922acc386f95b01f24da9fff8b
Lien <44433340+thailien@users.noreply.github.com>
2026-05-19 09:34:22 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://sachhaymoingay.netlify.app",
+  site: "https://sachhaymoingay.com",
   output: "static",
   trailingSlash: "always",
 });
